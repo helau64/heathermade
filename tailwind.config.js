afacba69cb6cdaec6af4c9b0ec2ea4c0afb4db72
heatherlauren <heatherlaurendavidson@gmail.com>
2023-01-05 12:55:36 +0000

    Handle font files and add Tailwind colour variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ["./**/*.njk"],
   theme: {
+    extend: {
+      colors: {
+        blue: 'rgb(50, 85, 164)',
+        green: 'rgb(0, 169, 92)',
+        red: 'rgb(255, 102, 94)',
+        sunflower: 'rgb(255, 181, 17)'
+      }
+    },
     screens: {
       xxs: '350px',
       xs: '580px',
