32c4dc4a0f08658d1a570ecb6cd58b733604cc59
helau64 <heather@hactar.is>
2023-03-28 14:10:57 +0100

    Move admin folder, create note template, colour fixes

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,7 @@ module.exports = function (eleventyConfig) {
   // human readable date
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
-      "dd LLL yyyy"
+      "d LLLL yyyy"
     );
   });
   
